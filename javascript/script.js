2e8b2c46523cb24b43c101c966f61ccf8fd946c7
Alexander <DefaultRayZ@gmail.com>
2020-04-19 12:50:55 +0300

    Поэксперементировал с примерами нейронки, и оптимизировал под некоторые моибльные устройства

--- javascript/script.js
+++ javascript/script.js
@@ -5,3 +5,20 @@ function slowScroll(id) {
   }, 1500)
   return false;
 }
+
+// <script>
+// $(window).scroll(function(){
+//   var scroll = $(window).scrollTop(),
+//   dh = $(document).height(),
+//   wh = $(window).height();
+//   scrollPercent = (scroll / (dh - wh)) * 100;
+//   $('#proggresBar').css('height', scrollPercent + '%');
+// })
+// </script>
+
+let proggres = document.getElementById('proggresBar');
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function(){
+  let progressHeight = (window.pageYOffset / totalheight) * 100;
+  progress.style.height = progresHeight + "%";
+}
